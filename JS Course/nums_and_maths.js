66a325fab88324c03fc4a88f9d6cc0f3f7e7b9dc
biraj2692 <77262608+biraj2692@users.noreply.github.com>
2024-07-20 19:21:23 +0000

    Updated JS Course

--- JS Course/nums_and_maths.js
+++
@@ -1,9 +0,0 @@
-const num1 = 100;
-console.log(num1);
-const bal =new Number(102);
-console.log(bal);
-console.log(bal.toFixed(2));
-
-const num2 = 1234.45647;
-console.log(num2.toPrecision(4));
-
---
+++ JS_Course/nums_and_maths.js
@@ -0,0 +1,32 @@
+// const num1 = 100;
+// console.log(num1);
+// const bal =new Number(102);
+// console.log(bal);
+// console.log(bal.toFixed(2));
+
+// const num2 = 1234.45647;
+// console.log(num2.toPrecision(4));
+
+// const hundred = 100000000;
+// console.log(hundred.toLocaleString('en-IN'));
+ 
+// console.log(Number.MAX_VALUE());
+
+// console.log(Math);
+// console.log(Math.floor(2.95));
+// console.log(Math.ceil(2.15));
+// console.log(Math.abs(-5));
+// console.log(Math.round(-5.55));
+
+// console.log(Math.random()); // 0 to 1
+// console.log((Math.random()*10) + 1); // 1 - 10
+// console.log(Math.floor(Math.random()*10) + 1); // integer only from 1-10
+
+// Formula
+
+let min = 1;
+let max = 6;
+
+let diceNum = Math.floor(Math.random() * (max - min + 1) + min);
+console.log(diceNum);
+
