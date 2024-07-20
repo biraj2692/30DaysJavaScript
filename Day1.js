c62035d767fba3878be5361777dacf04ea437892
biraj2692 <77262608+biraj2692@users.noreply.github.com>
2024-07-20 05:41:52 +0530

    Update Day1.js

--- Day1.js
+++ Day1.js
@@ -20,14 +20,41 @@ console.log(ans);
 
 // . Task 4: Create variables of different data types (number, string, boolean, object, array) and
 // log each variable's type using the typeof operator.
-
+let str = 'String';
+console.log(typeof str);
+let num = 123;
+console.log(typeof num);
+let bigint = 900719925124740999;
+console.log(typeof bigint);
+let bol = true;
+console.log(typeof bol);
+let ud = undefined;
+console.log(typeof ud);
+let nul = null;
+console.log(typeof nul);
+let student = {
+    firstName: "John",
+    lastName: null,
+    class: 10
+};
+console.log(typeof student);
 // Activity 4: Reassigning Variables
 
 // . Task 5: Declare a variable using 1et , assign it an initial value, reassign a new value, and log both values to the console.
-
+let a1 = "Biraj"
+console.log(a1)
+a1 = "Coder"
+console.log(a1);
 // Activity 5: Understanding const
 
 // . Task 6: Try reassigning a variable declared with const and observe the error.
+/*
+//TypeError: Assignment to constant variable.
+const a1 = "Biraj"
+console.log(a1)
+a1 = "Coder"
+console.log(a1);
+*/
 
 // Feature Request:
 // 1. Variable Types Console Log: Write a script that declares variables of different data types and logs both the value and type of each variable to the console.
